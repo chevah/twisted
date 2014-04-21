c1f748f2cf538158613ec9e731ff2389fcbc11d2
Adi Roiban <adi.roiban@chevah.com>
2014-04-21 15:07:14 +0300

    Try to wait.

--- twisted/internet/test/test_inotify.py
+++ twisted/internet/test/test_inotify.py
@@ -218,6 +218,8 @@ class TestINotify(unittest.TestCase):
         C{inotify.IN_DELETE_SELF} event to the callback.
         """
         def operation(path):
+            import time
+            time.sleep(1)
             path.remove()
 
         return self._notificationTest(
